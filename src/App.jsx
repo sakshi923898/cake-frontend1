fa3e6eefa638a3498ea006be41f52f8e7b3113da
sakshi923898 <satyavita81@gmail.com>
2025-10-06 18:24:37 +0530

    Add missing Owner model

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,40 @@
+// // src/App.jsx
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import CustomerDashboard from './components/CustomerDashboard';
+// import OwnerLogin from './components/OwnerLogin';
+// import OwnerDashboard from './components/OwnerPage';
+// import Order from './components/Order';
+// import ProtectedOwnerRoute from './utils/ProtectedOwnerRoute';
+
+// function App() {
+//   return (
+//     <>
+//       <Header />
+//       <Routes>
+//         {/* Customer routes */}
+//         <Route path="/" element={<CustomerDashboard />} />
+//         <Route path="/orders" element={<Order />} />
+
+//         {/* Owner routes */}
+//         <Route path="/owner/login" element={<OwnerLogin />} />
+//         <Route path="/ownerpage" element={
+//           <ProtectedOwnerRoute>
+//             <OwnerDashboard />
+//           </ProtectedOwnerRoute>
+//         } />
+//       </Routes>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
 // src/App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
@@ -20,11 +57,14 @@ function App() {
 
         {/* Owner routes */}
         <Route path="/owner/login" element={<OwnerLogin />} />
-        <Route path="/ownerpage" element={
-          <ProtectedOwnerRoute>
-            <OwnerDashboard />
-          </ProtectedOwnerRoute>
-        } />
+        <Route
+          path="/owner/dashboard"
+          element={
+            <ProtectedOwnerRoute>
+              <OwnerDashboard />
+            </ProtectedOwnerRoute>
+          }
+        />
       </Routes>
       <Footer />
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,47 @@
+// // src/main.jsx
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import App from "./App";
+// import Order from "./components/Order";
+// import OwnerLogin from "./components/OwnerLogin";
+// import OwnerDashboard from "./components/OwnerPage";
+// import ProtectedOwnerRoute from "./utils/ProtectedOwnerRoute";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       {/* App includes CustomerDashboard, Header, Footer etc. */}
+//       <Route path="*" element={<App />} />
+
+//       {/* Standalone order route */}
+//       <Route path="/orders" element={<Order />} />
+
+//       {/* Owner login route */}
+//       <Route path="/owner/login" element={<OwnerLogin />} />
+
+//       {/* Owner protected dashboard route */}
+//       <Route path="/owner/dashboard" element={
+//         <ProtectedOwnerRoute>
+//           <OwnerDashboard />
+//         </ProtectedOwnerRoute>
+//       } />
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Order from "./components/Order";
-import OwnerLogin from "./components/OwnerLogin";
-import OwnerDashboard from "./components/OwnerPage";
-import ProtectedOwnerRoute from "./utils/ProtectedOwnerRoute";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <BrowserRouter>
-    <Routes>
-      {/* App includes CustomerDashboard, Header, Footer etc. */}
-      <Route path="*" element={<App />} />
-
-      {/* Standalone order route */}
-      <Route path="/orders" element={<Order />} />
-
-      {/* Owner login route */}
-      <Route path="/owner/login" element={<OwnerLogin />} />
-
-      {/* Owner protected dashboard route */}
-      <Route path="/owner/dashboard" element={
-        <ProtectedOwnerRoute>
-          <OwnerDashboard />
-        </ProtectedOwnerRoute>
-      } />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
